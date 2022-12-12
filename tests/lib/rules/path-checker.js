2fb0d3b95d9efabf9bc3c035755ed2bbc1bfa2e6
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-12-12 05:39:59 +0200

    add properties to paths checker

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -2,30 +2,46 @@
  * @fileoverview feature sliced relative path checker
  * @author timur
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/path-checker"),
-  RuleTester = require("eslint").RuleTester;
-
+const rule = require('../../../lib/rules/path-checker'),
+  RuleTester = require('eslint').RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
-ruleTester.run("path-checker", rule, {
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
+ruleTester.run('path-checker', rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
-      code: "asfsa",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: 'В рамках одного слайса все пути должны быть относительными' }],
+      options: [
+        {
+          alias: '@',
+        },
+      ],
+    },
+    {
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: 'В рамках одного слайса все пути должны быть относительными' }],
     },
   ],
 });
